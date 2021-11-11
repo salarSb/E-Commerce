ab17e69b79a0ae875e573389fca0c87895562665
salar <salarsabeti88@gmail.com>
2021-11-11 22:53:58 +0330

    adding routes for admin's market and directories of admin view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AminDashboardController;
+use App\Http\Controllers\Admin\Market\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,15 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('/admin')->namespace('Admin')->name('admin.')->group(function () {
     Route::get('/', [AminDashboardController::class, 'index'])->name('home');
+    Route::prefix('/market')->namespace('Market')->name('market.')->group(function () {
+        //category
+        Route::prefix('category')->name('category.')->group(function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('index');
+            Route::get('/create', [CategoryController::class, 'create'])->name('create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('store');
+            Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('edit');
+            Route::put('/update/{id}', [CategoryController::class, 'update'])->name('update');
+            Route::put('/delete/{id}', [CategoryController::class, 'destroy'])->name('destroy');
+        });
+    });
 });
