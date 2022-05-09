0b2eaac4d13e04ba55207c37647a01c8ed056e1b
salar <salarsabeti88@gmail.com>
2022-05-09 10:45:52 +0430

    add useful helpers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -56,6 +56,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting()
     {
+        // todo: read from env or conf
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
