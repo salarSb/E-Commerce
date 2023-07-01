ad0d5a73ba33460db07d27dba6713b5e7fdf09b4
salar <salarsabeti88@gmail.com>
2023-07-01 20:15:15 +0330

    send emails by queue part2

---
+++ app/Jobs/SendEmailToUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Services\Message\Email\MailViewProvider;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmailToUser implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public array  $detail,
+        public string $subject,
+        public array  $from,
+        public string $to
+    )
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->to)->send(new MailViewProvider($this->detail, $this->subject, $this->from));
+    }
+}
